97027e10df34efbc053d314820ec92e237334271
Livvie Lin <livvie.lin@gmail.com>
2015-09-19 16:38:39 -0700

    Allows users to submit links

--- app.js
+++ app.js
@@ -15,8 +15,12 @@ app.controller('MainCtrl', function ($scope) {
       if (!$scope.title || $scope.title === '') {
         return;
       }
-      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0});
       $scope.title = '';
+      $scope.link = '';
     };
 
     $scope.incrementUpvotes = function (post) {
