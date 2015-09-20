d66a175ce690b4dfa0658a5f6d8710374b58aad8
Livvie Lin <livvie.lin@gmail.com>
2015-09-19 17:20:07 -0700

    Adds posts factory

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var app = angular.module('flapperNews', []);
 
-app.controller('MainCtrl', function ($scope) {
+app.factory('posts', [function () {
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
+
+app.controller('MainCtrl', function ($scope, posts) {
     $scope.test = 'Hello world!';
 
-    $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}];
+    $scope.posts = posts.posts;
 
     $scope.addPost = function () {
       // prevents user from submitting a blank post
