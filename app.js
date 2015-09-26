54f1c168874913460c55fcbf579d98f261c9a1ed
Livvie Lin <livvie.lin@gmail.com>
2015-09-25 21:00:02 -0700

    Refactor controller to utilize factory

--- app.js
+++ app.js
@@ -4,29 +4,43 @@ app.factory('posts', function () {
   var posts = {
     posts: []
   };
-  return posts;
+
+  var addPost = function (post) {
+    posts.posts.push(post);
+  };
+
+  var incrementUpvotes = function (post) {
+    post.upvotes += 1;
+  };
+
+  return {
+    posts: posts,
+    addPost: addPost,
+    incrementUpvotes: incrementUpvotes
+  };
 });
 
 app.controller('MainCtrl', function ($scope, posts) {
-    $scope.test = 'Hello world!';
 
-    $scope.posts = posts.posts;
+    $scope.posts = posts.posts.posts;
 
     $scope.addPost = function () {
       // prevents user from submitting a blank post
       if (!$scope.title || $scope.title === '') {
         return;
       }
-      $scope.posts.push({
+      // call factory post function
+      posts.addPost({
         title: $scope.title,
         link: $scope.link,
         upvotes: 0
       });
+      // reset input boxes
       $scope.title = '';
       $scope.link = '';
     };
 
     $scope.incrementUpvotes = function (post) {
-      post.upvotes += 1;
+      posts.incrementUpvotes(post);
     };
 });
